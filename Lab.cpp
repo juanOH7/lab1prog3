a5a0b40263c712f1985f69b2137666dab65951a8
juanoh <juanoh@unitec.edu>
2016-01-29 15:55:07 -0600

    Validaciones

--- Lab.cpp
+++ Lab.cpp
@@ -6,6 +6,7 @@ using std::cin;
 using std::endl;
 
 unsigned long long int factorial( int arg);
+bool validarCoord(int coordX1,int coordX2,int coordX3,int coordX4,int coordY1,int coordY2,int coordY3,int coordY4);
 
 int main(int argc, char*argv[]){
 	const unsigned long long int limFact=23;
@@ -19,8 +20,8 @@ int main(int argc, char*argv[]){
 		eulerNUM += (pow(numUsuario,i))/factorial(i);
 	}
 	cout<<"Resultado: "<<eulerNUM<<endl;
-	cout<<"Pitágoras ;-;"
-	double coordX1,coordX2,coordX3,coordX4,coordY1,coordY2,coordY3,coordY4;
+	cout<<"Pitágoras ;-;";
+	int coordX1,coordX2,coordX3,coordX4,coordY1,coordY2,coordY3,coordY4;
 	cout<<"X1:";
 	cin>>coordX1;
 	cout<<"X2:";
@@ -37,9 +38,41 @@ int main(int argc, char*argv[]){
 	cin>>coordY3;
 	cout<<"Y4:";
 	cin>>coordY4;
+	if (!validarCoord(coordX1,coordX2,coordX3,coordX4,coordY1,coordY2,coordY3,coordY4))
+	{
+		cout<<"Error en los puntos";
+	}
 	return 0;
 }
-
+bool validarCoord(int coordX1,int coordX2,int coordX3,int coordX4,int coordY1,int coordY2,int coordY3,int coordY4){
+	//X Val
+	if (coordX1==coordX2 && coordX2==coordX3)
+	{
+		return false;
+	}else if(coordX2==coordX3 && coordX3==coordX4){
+		return false;
+	}else if (coordX1==coordX2 && coordX2==coordX4)
+	{
+		return false;
+	}else if (coordX1==coordX3 && coordX3==coordX4)
+	{
+		return false;//Y Val
+	}else if (coordY1==coordY2 && coordY2==coordY3)
+	{
+		return false;
+	}else if (coordY2==coordY3 && coordY3==coordY4)
+	{
+		return false;
+	}else if (coordY1==coordY2 && coordY2==coordY4)
+	{
+		return false;
+	}else if (coordY1==coordY3 && coordY3==coordY4)
+	{
+		return false;
+	}else{
+		return true;
+	}
+}
 unsigned long long int factorial(int arg){
 	if (arg==0)
 	{
