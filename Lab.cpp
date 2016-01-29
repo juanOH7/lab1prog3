704c7aa200dbef0f51ad998c8faf0721e34659e7
juanoh <juanoh@unitec.edu>
2016-01-29 16:54:13 -0600

    perimetros

--- Lab.cpp
+++ Lab.cpp
@@ -57,13 +57,28 @@ int main(int argc, char*argv[]){
 	cout<<"Lado 3--"<<dist34<<endl;
 	dist24=distancia(coordX2,coordY2,coordX4,coordY4);
 	cout<<"Lado 4--"<<dist24<<endl;
+	//Trig 1
 	cout<<"Lados del Triángulo 1:"<<endl;
 	dist12=distancia(coordX1,coordY1,coordX2,coordY2);
 	cout<<"Lado 1--"<<dist12<<endl;
-	dist13=distancia(coordX1,coordY1,coordX3,coordY3);
-	cout<<"Lado 2--"<<dist13<<endl;
+	dist24=distancia(coordX2,coordY2,coordX4,coordY4);
+	cout<<"Lado 2--"<<dist24<<endl;
 	distDiag14=distancia(coordX1,coordY1,coordX4,coordY4);
 	cout<<"Lado 3--"<<distDiag14<<endl;	
+	//Trig 2
+	cout<<"Lados del Triángulo 2:"<<endl;
+	dist13=distancia(coordX1,coordY1,coordX3,coordY3);
+	cout<<"Lado 1--"<<dist13<<endl;
+	dist34=distancia(coordX3,coordY3,coordX4,coordY4);
+	cout<<"Lado 2--"<<dist34<<endl;
+	distDiag14=distancia(coordX1,coordY1,coordX4,coordY4);
+	cout<<"Lado 3--"<<distDiag14<<endl;
+	//PErSemiPer
+	double semiPer1,semiPer2;
+	semiPer1= perimetro(dist12,dist24,distDiag14);
+	cout<<"Semiperimetro Tri 1: "<<semiPer1<<endl;
+	semiPer2 = perimetro(dist13,dist34,distDiag14);
+	cout<<"Semiperimetro Tri 2: "<<semiPer2<<endl;
 	return 0;
 }
 bool validarCoord(double coordX1V,double coordX2V,double coordX3V,double coordX4V,double coordY1V,double coordY2V,double coordY3V,double coordY4V){
@@ -100,6 +115,10 @@ double distancia(double coordX1D,double coordY1D,double coordX2D,double coordY2D
 	int xD=coordX2D-coordX1D,yD=coordY2D-coordY1D;
 	dist= sqrt((pow(xD,2))+(pow(yD,2)));
 	return dist;
+}
+double perimetro(double dist1,double dist2,double dist3){
+	double per = (dist1+dist2+dist3)/2;
+	return per;
 }
 unsigned long long int factorial(int arg){
 	if (arg==0)
