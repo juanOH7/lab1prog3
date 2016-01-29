901fbc4b9800e7a3e3d737fcb628d3a369a622d2
juanoh <juanoh@unitec.edu>
2016-01-29 15:32:29 -0600

    Ejercicio 2,

--- Lab.cpp
+++ Lab.cpp
@@ -5,23 +5,42 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-int factorial(unsigned long int arg);
+unsigned long long int factorial( int arg);
 
 int main(int argc, char*argv[]){
-	const unsigned long int limFact=23;
+	const unsigned long long int limFact=23;
+	cout<<"Menú";
 	double eulerNUM, numUsuario;
 	cout<<"1-Euler XD";
-	cout<<"Numero :"<<endl;
+	cout<<"Número :"<<endl;
 	cin>>numUsuario;
 	for (int i = 0; i < limFact; i++)
 	{
 		eulerNUM += (pow(numUsuario,i))/factorial(i);
 	}
 	cout<<"Resultado: "<<eulerNUM<<endl;
+	cout<<"Pitágoras ;-;"
+	double coordX1,coordX2,coordX3,coordX4,coordY1,coordY2,coordY3,coordY4;
+	cout<<"X1:";
+	cin>>coordX1;
+	cout<<"X2:";
+	cin>>coordX2;
+	cout<<"X3:";
+	cin>>coordX3;
+	cout<<"X4:";
+	cin>>coordX4;
+	cout<<"Y1:";
+	cin>>coordY1;
+	cout<<"Y2:";
+	cin>>coordY2;
+	cout<<"Y3:";
+	cin>>coordY3;
+	cout<<"Y4:";
+	cin>>coordY4;
 	return 0;
 }
 
-int factorial(unsigned long int arg){
+unsigned long long int factorial(int arg){
 	if (arg==0)
 	{
 		return 1;
